51895960a13473b87d3225cbb23b4f3e0764fbfb
Johansson Johannes <a002028@ad.smhi.se>
2020-07-09 15:59:01 +0200

    updates

--- ctdvis/__init__.py
+++ ctdvis/__init__.py
@@ -15,20 +15,19 @@ name = "ctdvis"
 package_path = os.path.dirname(os.path.realpath(__file__))
 package_path = package_path.replace('ctdvis\\ctdvis', 'ctdvis')
 
-pypath_path = package_path + '\\pypaths.json'
+pypaths_path = package_path + '\\pypaths.json'
 
 
 def append_path_to_system(item):
     """"""
     if isinstance(item, str):
         if item not in sys.path:
-            print(item)
             if os.path.isdir(item):
                 sys.path.append(item)
             else:
                 print('\nWARNING! Could not add "{}" to sys.path. \n'
                       'You should probably change path to the py-package "{}" in '
-                      'settingsfile: {}\n'.format(item, os.path.basename(item), pypath_path))
+                      'settingsfile: {}\n'.format(item, os.path.basename(item), pypaths_path))
     elif isinstance(item, list):
         for it in item:
             append_path_to_system(it)
@@ -38,8 +37,8 @@ def append_path_to_system(item):
 
 
 def append_python_paths():
-    if os.path.isfile(pypath_path):
-        with open(pypath_path, 'r') as fd:
+    if os.path.isfile(pypaths_path):
+        with open(pypaths_path, 'r') as fd:
             d = json.load(fd)
         append_path_to_system(d)
 
