fc4a2f103fcf8ebdbef0c3195b103a4b99243fcc
Johansson Johannes <a002028@ad.smhi.se>
2021-03-17 15:43:05 +0100

    Create run_subprocess.py

---
+++ ctdvis/tests/run_subprocess.py
@@ -0,0 +1,25 @@
+# Copyright (c) 2020 SMHI, Swedish Meteorological and Hydrological Institute 
+# License: MIT License (see LICENSE.txt or http://opensource.org/licenses/mit).
+"""
+Created on 2021-03-17 15:27
+@author: johannes
+
+https://www.kite.com/python/answers/how-to-terminate-a-subprocess-in-python
+"""
+import subprocess
+
+
+if __name__ == "__main__":
+    """ Run bokeh-server via subprocess 
+
+        command: 
+            "bokeh", "serve"
+
+        python-script to run: 
+            "app_to_serve.py"  (or PATH TO PY-FILE)
+    """
+    child_process = subprocess.Popen(["bokeh", "serve", "app_to_serve.py"])
+
+    """ Terminate the subprocess (via GUI?) """
+    # child_process.terminate()
+
